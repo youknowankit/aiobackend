2353271824efe810e2943f9297de4ee945d285eb
Ankit Yadav <thisisankityadav@gmail.com>
2026-04-23 20:50:14 +0530

    Update CORS origin to include Netlify app
    Whitelisted frontend origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://retailkart.netlify.app"],
     credentials: true,
   }),
 );
